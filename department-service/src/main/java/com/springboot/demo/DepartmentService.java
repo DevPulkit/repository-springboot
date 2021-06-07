d30d73a014876aa9ad1179f2a80e5671e7cc62a6
DevPulkit <83303429+DevPulkit@users.noreply.github.com>
2021-06-07 19:24:19 +0530

    Microservice-Department

---
+++ department-service/src/main/java/com/springboot/demo/DepartmentService.java
@@ -0,0 +1,22 @@
+package com.springboot.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public Department saveDepartment(Department department) {
+        //log.info("Inside saveDepartment of DepartmentService");
+        return departmentRepository.save(department);
+    }
+
+    public Department findDepartmentById(Long departmentId) {
+        //log.info("Inside saveDepartment of DepartmentService");
+        return departmentRepository.findByDepartmentId(departmentId);
+    }
+}
