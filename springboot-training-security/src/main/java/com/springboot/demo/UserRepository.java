6e5871f9d85f305b02bedcbb396e13ce6ebb1693
DevPulkit <83303429+DevPulkit@users.noreply.github.com>
2021-06-07 15:14:25 +0530

    Getting- Error Executing DDL

---
+++ springboot-training-security/src/main/java/com/springboot/demo/UserRepository.java
@@ -0,0 +1,9 @@
+package com.springboot.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String userName);
+}
